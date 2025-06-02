444be0e27fc182731ee490e49dbbe0c63bd2b6f2
Jeffermus <70596979+Jeffermus@users.noreply.github.com>
2025-06-02 19:36:23 +0200

    remove emojies on 11b and deleted dist/ wasnt needed

--- 13a/dist/resolvers/Author.js
+++
@@ -1,8 +0,0 @@
-import db from "../database/data.js";
-const Author = {
-    books: (parent, args, context, info) => {
-        const booksByAuthor = db.books.filter((book) => book.authorId === Number(parent.id));
-        return booksByAuthor;
-    }
-};
-export default Author;
--- 13a/dist/resolvers/Book.js
+++
@@ -1,8 +0,0 @@
-import db from "../database/data.js";
-const Book = {
-    author: (parent, args, context, info) => {
-        const foundAuthor = db.authors.find((author) => author.id === parent.authorId);
-        return foundAuthor;
-    }
-};
-export default Book;
